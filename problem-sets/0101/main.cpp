e00e4f74f8fa8400122db7a0da6d5455b195cb5a
Joseph Mate <mate.joseph@gmail.com>
2014-08-26 17:17:19 -0400

    boiler plate code done

--- problem-sets/0101/main.cpp
+++ problem-sets/0101/main.cpp
@@ -1,60 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unordered_map>
 
-void collatz_search(int a, int b);
-int collatz_len(int a);
+using namespace std;
+
+
+struct State {
+	unordered_map<int, int> blockToStack;
+	int * stacks;
+	int n;
+} ;
+
+void process(State state, char * action1, char *action2, int a, int b);
+
+void output(State state);
+
+void set(int * array, int cols, int r, int c, int value);
+
+int get(int * array, int cols, int r, int c);
 
 int main(int argc, const char *argv[])
 {
-	int n1;
-	int n2;
-	while (fscanf(stdin, "%d %d\n", &n1, &n2) > 0) {
-		collatz_search(n1,n2);
+	char action1[5];
+	char action2[5];
+	int n, a, b;
+	int i,j;
+
+	// read in how many stacks and blocks we have
+	fscanf(stdin, "%d\n", &n);
+
+	// initialize data structures
+	State s;
+	// C++ doesn't allow 2d arrays :(
+	s.stacks = new int[n*n];
+	s.n = n;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			set(s.stacks,n,i,j,-1) ;
+		}
+		
+		s.blockToStack[i] = i;
+		set(s.stacks,n,i,0,i) ;
+	}
+
+	while (fscanf(stdin, "%s %d %s %d\n", action1, &a, action2, &b) > 0) {
+		process(s, action1,action2,a,b);	
 	}	 
 	
+	output(s);
 
+	// cleaning up all things we allocated
+	for (i = 0; i < n; i++) {
+		//delete s.stacks[i];
+	}	 
+	
+	delete s.stacks;
 	return 0;
 }
 
-void collatz_search(int a, int b)
-{
-	int ret;
-	int i;
-	int max_len = 0;
-	int start = a;
-	int end = b;
 
-	if( a > b ) {
-		start = b;
-		end = a;
-	}
-
-	for (i = start; i <= end; i++) {
-		int len = collatz_len(i);
-		if(len > max_len) {
-			ret = i;
-			max_len = len;
-		}
-	}
-
-	printf("%d %d %d\n", a, b, max_len);
+void set(int * array, int cols, int r, int c, int value){
+	array[ r*cols + c ] = value;
 }
 
-int collatz_len(int a)
-{
-	int counter = 0;
-	while(true) {
-		counter = counter + 1;
+int get(int * array, int cols, int r, int c){
+	return array[ r*cols + c ];
+}
 
-		if(a == 1){
-			break;
+void process(State state, char * action1, char *action2, int a, int b) {
+
+}
+
+void output(State state) {
+	int i;
+	int j;
+
+	for (i = 0; i < state.n; i++) {
+		printf("%d:", i);
+		
+		for (j = 0; j < state.n && get(state.stacks,state.n, i,j) >= 0 ; j++) {
+			printf(" %d", get(state.stacks, state.n, i,j));
 		}
 
-		if( a % 2 == 1) {
-			a = 3*a + 1;
-		} else {
-			a = a / 2;
-		}
+		printf("\n");
 	}
-	return counter;
 }
