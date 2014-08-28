43ae3bab09b3a29b615f77427cf4d5ee7d5e2fd5
Joseph Mate <mate.joseph@gmail.com>
2014-08-27 20:46:29 -0400

    now reads problem 103 input

--- problem-sets/0103/main.cpp
+++ problem-sets/0103/main.cpp
@@ -8,21 +8,61 @@ using namespace std;
 
 #define DEBUG
 
+void solve_nest_boxes(int ** boxes, int n, int d);
 
+#ifdef DEBUG
+void debug_boxes(int ** boxes, int n, int d);
+#endif
 
 int main(int argc, const char *argv[])
 {
-	char action1[5];
-	char action2[5];
-	int n, a, b;
+	int numOfBoxes, dimension;
 
 	// read in how many stacks and blocks we have
-	fscanf(stdin, "%d\n", &n);
+	while(scanf("%d %d\n", &numOfBoxes, &dimension) > 0) {
+		
 
-	while (fscanf(stdin, "%s %d %s %d\n", action1, &a, action2, &b) > 0 && action1[0] != 'q') {
-	}	 
+		//init variables
+		int ** boxes = new int*[numOfBoxes];
+		for (int i = 0; i < numOfBoxes; i++) {
+			boxes[i] = new int[dimension];
+		}
+		
+		//copy data into variables
+		for (int i = 0; i < numOfBoxes; i++) {
+			for(int j = 0; j < dimension; j++) {
+				scanf("%d", &boxes[i][j]);
+			}
+			scanf("\n");
+		}
+
+		solve_nest_boxes(boxes, numOfBoxes, dimension);
+
+		// free stuff we allocated
+		for (int i = 0; i < numOfBoxes; i++) {
+			delete boxes[i];
+		}
+		delete boxes;
+#ifdef DEBUG
+		printf("=====================\n");
+#endif
+	}
 	
 	return 0;
 }
 
+
+void solve_nest_boxes(int ** boxes, int n, int d) {
+	debug_boxes(boxes, n, d);
+}
+
+void debug_boxes(int ** boxes, int n, int d) {
+	for(int i = 0 ; i < n; i++) {
+			printf("%d:", (i+1));
+		for(int j = 0 ; j < d; j++) {
+			printf("\t%d", boxes[i][j]);
+		}
+		printf("\n");
+	}
+}
 
