730c32d38f0f7ad73b51476fd028815bc6ab6e87
Joseph Mate <mate.joseph@gmail.com>
2014-08-28 17:37:32 -0400

    compiles after switching to Box* objects so that I can sort

--- problem-sets/0103/main.cpp
+++ problem-sets/0103/main.cpp
@@ -3,19 +3,51 @@
 #include <string.h>
 #include <unordered_map>
 #include <map>
+#include <set>
 
 using namespace std;
 
 #define DEBUG
 
-void solve_nest_boxes(int ** boxes, int n, int d);
+struct Box {
+
+	int * box;
+	int d;
+
+	Box(int d) {
+		box = new int[d];
+	}
+
+	~Box(){
+		delete box;
+	}
+
+
+};
+
+
+struct cmp {
+	bool operator()(Box* const& a, Box* const& b) const {
+		for(int i = 0; i < a->d; i++) {
+			if(a->box[i] > b->box[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+void solve_nest_boxes(Box ** boxes, int n, int d);
+map<Box*,int,cmp> sort_boxes(Box ** boxes, int n, int d);
+void sort_box(Box * box, int d) ;
 
 #ifdef DEBUG
-void debug_boxes(int ** boxes, int n, int d);
+void debug_boxes(Box ** boxes, int n, int d);
+void debug_box(Box * box, int d) ;
 #endif
 
-int main(int argc, const char *argv[])
-{
+
+int main(int argc, const char *argv[]) {
 	int numOfBoxes, dimension;
 
 	// read in how many stacks and blocks we have
@@ -23,15 +55,15 @@ int main(int argc, const char *argv[])
 		
 
 		//init variables
-		int ** boxes = new int*[numOfBoxes];
+		Box ** boxes = new Box*[numOfBoxes];
 		for (int i = 0; i < numOfBoxes; i++) {
-			boxes[i] = new int[dimension];
+			boxes[i] = new Box(dimension);
 		}
 		
 		//copy data into variables
 		for (int i = 0; i < numOfBoxes; i++) {
 			for(int j = 0; j < dimension; j++) {
-				scanf("%d", &boxes[i][j]);
+				scanf("%d", &boxes[i]->box[j]);
 			}
 			scanf("\n");
 		}
@@ -52,17 +84,66 @@ int main(int argc, const char *argv[])
 }
 
 
-void solve_nest_boxes(int ** boxes, int n, int d) {
+void solve_nest_boxes(Box ** boxes, int n, int d) {
+#ifdef DEBUG
 	debug_boxes(boxes, n, d);
+#endif
+
+	// i'm assuming that the fit IFF their sorted dimensions fit (without moving)
+	map<Box*,int,cmp> sortedBoxes = sort_boxes(boxes,n,d);
+
+#ifdef DEBUG
+	debug_boxes(boxes, n, d);
+#endif
 }
 
-void debug_boxes(int ** boxes, int n, int d) {
+
+map<Box*,int,cmp> sort_boxes(Box ** boxes, int n, int d) {
+	for (int i = 0; i < n; i++) {
+		sort_box(boxes[i], d);
+	}
+	
+	map<Box*, int,cmp> sortedBoxes;
+	for (int i = 0; i < n; i++) {
+		sortedBoxes[boxes[i]] = i+1;
+	}
+
+	return sortedBoxes;
+}
+
+void sort_box(Box * box, int d) {
+	int i;
+	set<int> sortedBox;
+	for (i = 0; i < d; i++) {
+		sortedBox.insert(box->box[i]);
+	}
+
+	i = 0;
+	for(set<int>::iterator itr = sortedBox.begin(); itr != sortedBox.end(); itr++) {
+		box[i] = *itr;
+		i++;
+	}
+}
+
+
+void debug_boxes(map<Box*,int,cmp> boxes, int n, int d) {
+	for(map<Box*,int,cmp>::iterator itr = boxes.begin(); itr != boxes.end(); itr++) {
+		printf("%d:", itr->second);
+		debug_box(itr->first, d);
+		printf("\n");
+	} 
+}
+
+void debug_boxes(Box ** boxes, int n, int d) {
 	for(int i = 0 ; i < n; i++) {
 			printf("%d:", (i+1));
-		for(int j = 0 ; j < d; j++) {
-			printf("\t%d", boxes[i][j]);
-		}
+		debug_box(boxes[i], d);
 		printf("\n");
 	}
 }
 
+void debug_box(Box * box, int d) {
+	for(int j = 0 ; j < d; j++) {
+		printf("\t%d", box->box[j]);
+	}
+}
