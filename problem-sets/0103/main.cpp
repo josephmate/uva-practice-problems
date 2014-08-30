32c03ed12814791e8a6bd30dc96be6d0ffd47b01
Joseph Mate <mate.joseph@gmail.com>
2014-08-29 23:38:43 -0400

    finished soln, but runs to slow - changed everything to reuse objects (no more full object copies): still too   slow - still thinking over what to try next

--- problem-sets/0103/main.cpp
+++ problem-sets/0103/main.cpp
@@ -3,10 +3,11 @@
 #include <string.h>
 #include <map>
 #include <set>
+#include <vector>
 
 using namespace std;
 
-#define DEBUG
+//#define DEBUG
 
 struct Box {
 
@@ -26,32 +27,18 @@ struct Box {
 };
 
 
-typedef struct cmp cmp;
 void solve_nest_boxes(Box ** boxes, int n, int d);
-void find_longest_chain(map<Box*,int,cmp> boxes, int d, int * longestLen, int * longestStartPosn) ;
+vector<int>* find_longest_chain(Box ** boxes, int n, int d) ;
+vector<int>* bfs(bool ** adjmat, int n) ;
 bool fits(Box* a, Box* b);
-map<Box*,int,cmp> sort_boxes(Box ** boxes, int n, int d);
+void sort_boxes(Box ** boxes, int n, int d);
 void sort_box(Box * box) ;
 
 #ifdef DEBUG
-void debug_boxes(map<Box*,int,cmp> boxes, int n, int d) ;
+void debug_adjmat(bool ** adjmat, int n);
 void debug_boxes(Box ** boxes, int n, int d);
 void debug_box(Box * box) ;
 #endif
-
-struct cmp {
-	bool operator()(Box* const& a, Box* const& b) const {
-		for(int i = 0; i < a->d; i++) {
-			if(a->box[i] < b->box[i]) {
-				return true; // a < b
-			} else if(a->box[i] > b->box[i]) {
-				return false; // b < a
-			}
-		}
-		return false; // equal
-	}
-};
-
 
 int main(int argc, const char *argv[]) {
 	int numOfBoxes, dimension;
@@ -96,83 +83,100 @@ void solve_nest_boxes(Box ** boxes, int n, int d) {
 #endif
 
 	// i'm assuming that the fit IFF their sorted dimensions fit (without moving)
-	map<Box*,int,cmp> sortedBoxes = sort_boxes(boxes,n,d);
+	sort_boxes(boxes,n,d);
 
 #ifdef DEBUG
 	debug_boxes(boxes, n, d);
-	debug_boxes(sortedBoxes, n, d);
 #endif
 
-	int length = 0;
-	int startPosn = -1;
-	find_longest_chain(sortedBoxes, d, &length, &startPosn) ;
-#ifdef DEBUG
-	printf("longest chain length: %d\n", length);
-	printf("start posn of chain: %d\n", startPosn);
-#endif
-
+	vector<int>* longestChain = find_longest_chain(boxes, n, d);
 
 	// output the longest chain that we found
+	printf("%lu\n", longestChain->size());
+	for (vector<int>::iterator itr = longestChain->begin() ; itr != longestChain->end(); ++itr){
+		if(itr != longestChain->begin()) {
+			printf(" ");
+		}
+		printf("%d", (*itr) + 1);
+	}
+	printf("\n");
 	
-
+	delete longestChain;
 }
 
 // find the longest chain of boxes that fit
-void find_longest_chain(map<Box*,int,cmp> boxes, int d, int * longestLen, int * longestStartPosn) {
-	*longestLen = 0;
-	*longestStartPosn = -1;
-	int currentLen = 0;
-	int i = 0;
-	int posn = 0;
-	Box* prev = NULL;
+vector<int>* find_longest_chain(Box ** boxes, int n, int d) {
+	bool ** adjmat = new bool*[n];
+	for (int i = 0; i < n; i++) {
+		adjmat[i] = new bool[n];
+	}
 
-	for(map<Box*,int,cmp>::iterator itr = boxes.begin(); itr != boxes.end(); itr++) {
-
-		if(prev == NULL) {
-			posn = 0;
-			currentLen = 1;
-#ifdef DEBUG
-			printf("i %d, origPosn %d, start %d, currentLen %d, START ", i, itr->second, posn, currentLen);
-			debug_box(itr->first);
-			printf("\n");
-#endif
-		} else if(fits(prev,itr->first)) {
-#ifdef DEBUG
-			printf("i %d, origPosn %d, start %d, currentLen %d, FITS  ", i, itr->second, posn, currentLen);
-			debug_box(itr->first);
-			printf("\n");
-#endif
-			currentLen++;
-		} else {
-#ifdef DEBUG
-			printf("i %d, origPosn %d, start %d, currentLen %d, START ", i, itr->second, posn, currentLen);
-			debug_box(itr->first);
-			printf("\n");
-#endif
-			// The previous box did not fit.
-			//
-			// We check if the previous chain is longer than the longest chain so far.
-			//
-			// As a result, we reset the the counters
-			// and have this guy as the starting posn.
-			
-			if(currentLen > *longestLen) {
-				*longestLen = currentLen;
-				*longestStartPosn = posn;
-			}
-
-			posn = i;
-			currentLen = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			adjmat[i][j] = fits(boxes[i],boxes[j]);
 		}
-		
-		i++;
-		prev = itr->first;
 	}
-	if(currentLen > *longestLen) {
-		*longestLen = currentLen;
-		*longestStartPosn = posn;
+#ifdef DEBUG 
+	debug_adjmat(adjmat,n);
+#endif
+
+	vector<int>* result = bfs(adjmat, n);
+
+	for (int i = 0; i < n; i++) {
+		delete [] adjmat[i];
 	}
+	delete [] adjmat;
+
+	return result;
 }
+
+vector<int>* bfs_impl(bool ** adjmat, int n, set<int> * visited, int current, int depth) {
+	int max = 0;
+	vector<int> * best = NULL;
+
+	// try each of the directions
+	for (int i = 0; i < n; i++) {
+		if(visited->find(i) == visited->end() && adjmat[i][current]) {
+			visited->insert(i);
+			vector<int> * attempt = bfs_impl(adjmat, n, visited, i, depth + 1);
+			if(attempt->size() > max) {
+				best = attempt;
+				max = attempt->size();
+			} else {
+				delete attempt;
+			}
+			visited->erase(i);
+		}
+	}
+	
+	if(best == NULL) {
+		best = new vector<int>();
+	}	
+	best->push_back(current);
+	return best;
+}
+
+// depth first search
+vector<int>* bfs(bool ** adjmat, int n) {
+	unsigned int max = 0;
+	vector<int> * best;
+
+	for (int i = 0; i < n; i++) {
+		set<int> visited;
+		visited.insert(i);
+		vector<int>* attempt = bfs_impl(adjmat, n, &visited, i, 1);
+		if(attempt->size() > max) {
+			best = attempt;
+			max = attempt->size();
+		} else {
+			delete attempt;
+		}
+	}
+
+	return best;
+}
+
+
 
 bool fits(Box* a, Box* b) {
 	for (int i = 0; i < a->d; i++) {
@@ -184,17 +188,10 @@ bool fits(Box* a, Box* b) {
 	return true;
 }
 
-map<Box*,int,cmp> sort_boxes(Box ** boxes, int n, int d) {
+void sort_boxes(Box ** boxes, int n, int d) {
 	for (int i = 0; i < n; i++) {
 		sort_box(boxes[i]);
 	}
-	
-	map<Box*, int,cmp> sortedBoxes;
-	for (int i = 0; i < n; i++) {
-		sortedBoxes[boxes[i]] = i+1;
-	}
-
-	return sortedBoxes;
 }
 
 void sort_box(Box * box) {
@@ -211,14 +208,14 @@ void sort_box(Box * box) {
 	}
 }
 
-
-void debug_boxes(map<Box*,int,cmp> boxes, int n, int d) {
-	printf("AoAoA\n");
-	for(map<Box*,int,cmp>::iterator itr = boxes.begin(); itr != boxes.end(); itr++) {
-		printf("%d:", itr->second);
-		debug_box(itr->first);
+#ifdef DEBUG
+void debug_adjmat(bool ** adjmat, int n) {
+	for(int i = 0 ; i < n; i++) {
+		for(int j = 0 ; j < n; j++) {
+			printf("%d\t", (int)adjmat[i][j]);
+		}
 		printf("\n");
-	} 
+	}
 }
 
 void debug_boxes(Box ** boxes, int n, int d) {
@@ -235,3 +232,4 @@ void debug_box(Box * box) {
 		printf("\t%d", box->box[j]);
 	}
 }
+#endif
