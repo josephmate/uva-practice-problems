3e8503299951dd15b6692e4c67a7dc60cf5e5efc
Joseph Mate <mate.joseph@gmail.com>
2014-09-05 15:33:52 -0400

    reading 104 ready

--- problem-sets/0104/main.cpp
+++ problem-sets/0104/main.cpp
@@ -10,22 +10,68 @@
 using namespace std;
 
 #define DEBUG
+void solve_exrates(double ** exrates, int d);
 
-
+#ifdef DEBUG
+void debug_exrates(double ** exrates, int d);
+#endif
 
 int main(int argc, const char *argv[])
 {
-	char action1[5];
-	char action2[5];
-	int n, a, b;
+	int dimension;
 
 	// read in how many stacks and blocks we have
-	fscanf(stdin, "%d\n", &n);
+	while(scanf("%d\n", &dimension) > 0) {
 
-	while (fscanf(stdin, "%s %d %s %d\n", action1, &a, action2, &b) > 0 && action1[0] != 'q') {
-	}	 
+		//init variables
+		double ** exrates = new double*[dimension];
+		for (int i = 0; i < dimension; i++) {
+			exrates[i] = new double[dimension];
+		}
+		
+		//copy data into variables
+		for (int i = 0; i < dimension; i++) {
+			for(int j = 0; j < dimension; j++) {
+				if(i == j){
+					exrates[i][j] = 1.0;
+				} else {
+					scanf("%lf", &exrates[i][j]);
+				}
+			}
+			scanf("\n");
+		}
+
+		solve_exrates(exrates, dimension);
+
+
+		// free stuff we allocated
+		for (int i = 0; i < dimension; i++) {
+			delete [] exrates[i];
+		}
+		delete [] exrates;
+#ifdef DEBUG
+		printf("=====================\n");
+#endif
+	}
 	
 	return 0;
 }
 
+void solve_exrates(double ** exrates, int d) {
+#ifdef DEBUG
+	debug_exrates(exrates,d);
+#endif
+}
 
+#ifdef DEBUG
+void debug_exrates(double ** exrates, int d) {
+		for (int i = 0; i < d; i++) {
+			for(int j = 0; j < d; j++) {
+				printf("%lf\t", exrates[i][j]);
+			}
+			printf("\n");
+		}
+
+}
+
+#endif
